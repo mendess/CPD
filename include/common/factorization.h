68b195663801ec41ffbcb3a6f129cc6657d7cb49
Filipe Lucas <filipe.jl.ines@gmail.com>
2020-03-06 22:51:21 +0000

    factorization

---
+++ include/common/factorization.h
@@ -0,0 +1,1 @@
+double delta(double const A, double const B, double const LR);
---
+++ src/common/factorization.c
@@ -0,0 +1,4 @@
+#include "factorization.h"
+double delta(double const A, double const B, double const LR){
+    return 2 * (A - B) * (-LR);
+}
