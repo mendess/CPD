34253772855ce33a02ed56b760d04637f19552aa
Mendess2526 <pedro.mendes.26@gmail.com>
2020-03-19 11:59:57 +0000

    Remove debug prints

--- src/serial/matFact.c
+++ src/serial/matFact.c
@@ -31,13 +31,6 @@ void next_iter_l(Matrices const* matrices, Matrix* aux_l, Matrix const* b) {
             while (a_iter.iter != a_iter.end) {
                 CMatrixIterRowItem a_iter_item = cmatrix_iter_row_next(&a_iter);
                 size_t const j = a_iter_item.column;
-                fprintf(
-                    stderr,
-                    "(i,j,k) = (%zu,%zu,%zu) <> %f\n",
-                    i,
-                    j,
-                    k,
-                    *a_iter_item.value);
                 aux += delta(
                     *a_iter_item.value,
                     *matrix_at(b, i, j),
@@ -58,13 +51,6 @@ void next_iter_r(Matrices const* matrices, Matrix* aux_r, Matrix const* b) {
             while (a_iter.iter != a_iter.end) {
                 CMatrixIterRowItem a_iter_item = cmatrix_iter_row_next(&a_iter);
                 size_t const i = a_iter_item.column;
-                fprintf(
-                    stderr,
-                    "(i,j,k) = (%zu,%zu,%zu) <> %f\n",
-                    i,
-                    j,
-                    k,
-                    *a_iter_item.value);
                 aux += delta(
                     *a_iter_item.value,
                     *matrix_at(b, i, j),
@@ -86,32 +72,14 @@ Matrix iter(Matrices* matrices) {
     Matrix aux_l = matrix_make(matrices->l.rows, matrices->l.columns);
     Matrix aux_r = matrix_make(matrices->r.rows, matrices->r.columns);
     Matrix b = matrix_make(matrices->a_prime.n_rows, matrices->a_prime.n_cols);
-    fprintf(stderr, "Initial matrix A\n");
-    cmatrix_print(&matrices->a_prime);
-    fprintf(stderr, "Initial matrix L\n");
-    matrix_print(&matrices->l);
-    fprintf(stderr, "Initial matrix R\n");
-    matrix_print(&matrices->r);
-    fprintf(stderr, "Initial matrix B\n");
-    matrix_print(&b);
     for (size_t i = 0; i < matrices->num_iterations; i++) {
         if (i != 0) matrix_clear(&b); // TODO: benchmark
         matrix_b(&matrices->l, &matrices->r, &b);
-        fprintf(stderr, "iter_l\n");
         next_iter_l(matrices, &aux_l, &b);
-        fprintf(stderr, "iter_r\n");
         next_iter_r(matrices, &aux_r, &b);
-        /* char b[40]; */
-        /* fgets(b, 40, stdin); */
         swap(&matrices->l, &aux_l);
         swap(&matrices->r, &aux_r);
     }
-    fprintf(stderr, "Matrix L\n");
-    matrix_print(&matrices->l);
-    fprintf(stderr, "Matrix R\n");
-    matrix_print(&matrices->r);
-    fprintf(stderr, "Matrix B\n");
-    matrix_print(&b);
     matrix_free(&aux_l);
     matrix_free(&aux_r);
     return b;
